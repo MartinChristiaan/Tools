861490173da2cb5af3655266a3efb5850ce29e1a
leeuwenmcv <martin.vanleeuwen@tno.nl>
2023-08-25 17:54:48 +0200

    changed rc

--- bash_extension/bash_extension.sh
+++ bash_extension/bash_extension.sh
@@ -1,6 +1,7 @@
 export PATH="~/git/tools/bin:$PATH"
 export TOOLS=~/git/tools
-alias ex='source explorer'
+alias ex='source ~/git/tools/explorer2/explore.sh'
 alias x="bash ~/git/tools/executor/execute.sh"
 alias qo="cd ~/git/tools/quick_open/ && bash nautilus_quick_open.sh"
+
 
