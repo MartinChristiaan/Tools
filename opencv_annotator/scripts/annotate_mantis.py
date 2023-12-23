ec70c572271b1f68340c225cfce6c131e160f7a6
Master <martin@pop-os.localdomain>
2023-12-23 16:49:50 +0100

    auto commit

--- opencv_annotator/scripts/annotate_mantis.py
+++ opencv_annotator/scripts/annotate_mantis.py
@@ -7,6 +7,10 @@ from cache_annotator import IOManager
 from scripts.dataset_config import get_mantis
 
 mantis = get_mantis()
+from pre_annotation_writer import PreAnnotationWriter
+
+for config in mantis:
+	writer  = PreAnnotationWriter(config,[0,-15,15],source="tyolov8/tracks_tyolov8m-30112023.csv")
 import dlutils_ii as du
 
 annotator = BoundingBoxAnnotator(mantis)
