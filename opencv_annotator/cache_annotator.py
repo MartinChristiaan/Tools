c3d5e2bc468d0f06f5f2f6f74a7e9fbca7a8ff7d
Master <martin@pop-os.localdomain>
2023-12-27 20:28:15 +0100

    auto commit

--- opencv_annotator/cache_annotator.py
+++ opencv_annotator/cache_annotator.py
@@ -15,6 +15,20 @@ from state import Observable, State
 # %%
 
 
+def apply_ignore_areas(annotations: List[Annotation]):
+    ignore_areas = [x for x in annotations if x.label == "ignore_area"]
+    other_annotations = [x for x in annotations if x.label != "ignore_area"]
+    filtered_annotations = []
+    for a in other_annotations:
+        for ignore_area in ignore_areas:
+            print(a.cx, a.cy, ignore_area.is_inside(a.cx, a.cy))
+            if not ignore_area.is_inside(a.cx, a.cy)
+                filtered_annotations.append(a)
+    num_filtered_annotations = len(annotations) - len(filtered_annotations)
+    logger.debug(f"filtered {num_filtered_annotations} annotations")
+    return filtered_annotations
+
+
 class IOManager:
     def __init__(self, dataset_config: du.DatasetConfig, state: State) -> None:
         self.state = state
@@ -152,6 +166,7 @@ class IOManager:
                 ]
             )
         detections += self.tracked_annotations
+        detections = apply_ignore_areas(detections)
         frames = [
             cv2.imread(self.dataset_config.pathfinder.frame_filename(o, timestamp))
             for o in [0, -15, 15]
