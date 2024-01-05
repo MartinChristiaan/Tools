5a1b13a6594e24e3b5b32d677203049e7af5ed52
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-01-05 15:31:18 +0100

    auto commit

---
+++ opencv_annotator/scripts/annotate_air.py
@@ -0,0 +1,32 @@
+# %%
+%load_ext autoreload
+%autoreload 2
+
+
+from typing import List
+import dlutils_ii as du
+from annotator import BoundingBoxAnnotator
+from scripts.dataset_config import get_mantis
+
+import dlutils_ii as du
+
+from pathlib import Path
+from loguru import logger
+
+from videosets_ii.videosets_ii import VideosetsII
+from trackertoolbox.detections import Detections
+from trackertoolbox.tracks import Tracks, TrackUpdates
+import pandas as pd
+
+basedirpath = Path(r"/diskstation")
+videosets = VideosetsII(basedirpath=basedirpath)  # basedirpath)
+
+pf = du.Pathfinder(
+    "v2119_campaign1_flight01", camera="EO", quality="_nq", cache_dir="/data/sod_cache"
+)
+
+o = du.TrainOptions(
+    False, [1], max_samples=100, annotations_suffix="ignore"#, motion_models="compute"
+)
+tharde = du.DatasetConfig(pf, o)
+annotator = BoundingBoxAnnotator(tharde).run()
