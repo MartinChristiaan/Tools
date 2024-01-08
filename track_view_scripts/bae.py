838406711ab2c4cb43855b8206e89f97b7149588
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-01-08 11:01:55 +0100

    auto commit

--- track_view_scripts/bae.py
+++ track_view_scripts/bae.py
@@ -1,21 +1,3 @@
-"""
-Script to visualize track evaluation results on track level.
-Example set from II tracking course.
-
-For installation:
-- Python 3.9, other version can give problems
-- Create venv
-- Install GUI toolbox:
-    pip install git+https://toolboxes:glpat-TozmQ6kmAsaNB-M_sfQr@gitlab.tsn.tno.nl/intelligent_imaging/python/toolboxes.git#subdirectory=guitoolbox
-- Install reid toolbox (dependency hopefully will be removed in futer)
-    pip install git+https://toolboxes:glpat-TozmQ6kmAsaNB-M_sfQr@gitlab.tsn.tno.nl/intelligent_imaging/python/toolboxes.git#subdirectory=reidtoolbox
-- Install videosets_ii
-    pip install git+https://videosets_ii:glpat-Fns9WB-uccyz-pcuPXdx@gitlab.tsn.tno.nl/intelligent_imaging/python/data_management/videosets_ii.git
-
-Then run this script
-
-"""
-
 from videosets_ii.videosets_ii import VideosetsII
 import numpy as np
 
