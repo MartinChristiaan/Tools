32a19cfd27871a9eb7ef7c5b4ff1af8d142bf439
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-01-28 11:03:38 +0100

    auto commit

--- HourplanAnalysis/hourplan_analysis_mat.py
+++ HourplanAnalysis/hourplan_analysis_mat.py
@@ -1,8 +1,17 @@
 # %%
+import numpy as np
 from scipy.io import loadmat
 import os
 
+import streamlit as st
+from tqdm import tqdm
+
+# set wide layout
+st.set_page_config(layout="wide")
+
 home = os.path.expanduser("~")
+
+# TODO also load previous year, + automatically load newest
 data = loadmat(f"{home}/OurPlanDump_II_2024_240126.mat")
 # %%
 print(data.keys())
@@ -25,13 +34,37 @@ for i, e in enumerate(employees):
     print(e)
     break
 
-# get projects
+# %% get projects
 
-projects = [x[0][0] for x in data["WBS"]]
-print(projects)
-
-
-# print(data["Employees"][0][0])
+# streamlit selection
+st.title("Project planner")
 
 
-# print(data["RealisedHours"].shape)
+projects = [x[1][0] for x in data["WBS"]]
+
+planned_hours = data["PlannedHours"][:, :, employee_index]
+realised_hours = data["RealisedHours"][:, :, employee_index]
+
+
+# get project ids with planned or realised hours
+combined_hours = planned_hours + realised_hours
+combined_hours = combined_hours.sum(axis=0)
+active_projects = np.argwhere(combined_hours > 0)
+active_projects_labels = [projects[i[0]] for i in active_projects]
+project = st.selectbox("Project", active_projects_labels)
+project_index = projects.index(project)
+
+planned = np.cumsum(planned_hours[:, project_index])
+real = np.cumsum(active_projects[:, project_index])
+
+fig = go.Figure()
+fig.add_trace(
+    go.Scatter(
+        x=planned_dates[start_date:end_date], y=planned, mode="lines", name="Planned"
+    )
+)
+fig.add_trace(
+    go.Scatter(x=real_dates[start_date:end_date], y=real, mode="lines", name="Real")
+)
+fig.update_layout(title="Planned vs Real", xaxis_title="Date", yaxis_title="Hours")
+st.plotly_chart(fig, use_container_width=True)
