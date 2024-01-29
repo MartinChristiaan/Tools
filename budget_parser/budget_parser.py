ee26627aaeaae0026ce41c4cbbce2ad547bee369
Master <martin@pop-os.localdomain>
2024-01-29 21:00:02 +0100

    auto commit

--- budget_parser/budget_parser.py
+++ budget_parser/budget_parser.py
@@ -2,8 +2,10 @@
 # parse excel
 import datetime
 import os
+import re
 
 import pandas as pd
+from matplotlib import category
 
 home = os.path.expanduser("~")
 df = pd.read_excel(f"{home}/budget_2024.xls")
@@ -13,6 +15,14 @@ descriptions = df["description"].unique()
 from dataclasses import dataclass
 
 
+def clean_description(description: str):
+    "remove double spaces and make lowercase and remove non word characters except spaces"
+    # description = description.lower()
+    description = description.lower().replace("  ", " ")
+    description = re.sub(r"[^\w\s]", "", description)
+    return description
+
+
 @dataclass
 class Statement:
     description: str
@@ -20,8 +30,23 @@ class Statement:
     date: datetime.datetime
 
     @staticmethod
-    def from_df(df):
-        return [Statement(**row) for i, row in df.iterrows()]
+    def from_dict(dict):
+        date = dict["transactiondate"]
+        # parse 20231229 to datetime
+        date = datetime.datetime.strptime(str(date), "%Y%m%d")
+        return Statement(row["description"], row["amount"], date)
+
+    def __repr__(self) -> str:
+        return f"{clean_description(self.description)} : {self.amount} : {self.date}"
+
+
+statements = []
+for i, row in df.iterrows():
+    statements.append(Statement.from_dict(row))
+print(statements)
+
+
+# %%
 
 
 @dataclass
@@ -29,26 +54,39 @@ class BudgetCategory:
     name: str
     monthly: int
     keywords: list[str]
-    statements: list[str] = None
+    statements: list[Statement]
+
+    def __repr__(self) -> str:
+        statements = "\n".join([str(x) for x in self.statements])
+        return f"{self.name} : {self.monthly} \n {statements}"
 
 
 budget_categories = [
-    BudgetCategory("groceries", 400, ["jumbo", "hoogvliet", "albert", "samara"]),
+    BudgetCategory("groceries", 400, ["jumbo", "hoogvliet", "albert", "samara"], []),
+    BudgetCategory("other", 0, [], []),
 ]
 
 
-# def assign_category(description):
-#     current_cat = "other"
-#     for category in budget_categories:
-#         for keyword in category.keywords:
-#             if keyword in description:
-#                 if current_cat != "other":
-#                     raise ValueError(
-#                         f"Multiple categories found for description {description}"
-#                     )
-#                 current_cat = category.name
-#     return current_cat
+def assign_category(statement: Statement, budget_categories: list[BudgetCategory]):
+    category = None
+    for cat in budget_categories:
+        for keyword in cat.keywords:
+            if keyword in statement.description.lower():
+                if not category is None:
+                    raise Exception(
+                        f"{statement.description} is in multiple categories"
+                    )
+                cat.statements.append(statement)
+                category = cat
+                break
+    if category is None:
+        budget_categories[-1].statements.append(statement)
 
 
-# for i, row in df.iterrows():
-#     df.loc[i, "category"] = assign_category(row["description"])
+for statement in statements:
+    assign_category(statement, budget_categories)
+
+for cat in budget_categories:
+    print(cat)
+
+# %%
