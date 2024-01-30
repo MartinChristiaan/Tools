1f756f921031ff924b03ddc977d9a072a1de5ce4
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-01-30 18:24:23 +0100

    auto commit

--- budget_parser/pre_annotation.py
+++ budget_parser/pre_annotation.py
@@ -12,17 +12,51 @@ keyword_to_cat = {
     "rent": ["vastgoed"],
     "groceries": ["hoogvliet", "jumbo", "albert", "heijn"],
     "misc": ["bol.com"],
-    "vacation": ["wien", "vienna", "graz"],
     "ov": ["ov-chipkaart"],
     "bike": ["swapfiets"],
     "insurance": ["fbto"],
     "healthcare": ["infomedics"],
-    "fun": ["boulder"],
+    "bank": ["betaalpas"],
+    "fitness": ["basic fit", "bodyfit"],
+    "fun": [
+        "boulder",
+        "salsa",
+        "mcdonalds",
+        "betaalverzoek",
+    ],
     "clothes": ["h&m", "zalando"],
     "phone": ["kpn"],
     "tax": ["belastingdienst"],
+    "electricity": ["greenchoice"],
+    "water": ["dunea"],
+    "vacation": [
+        "odigeo",
+        "booking.com",
+        "airbnb",
+        "easyjet",
+        "ryanair",
+        "klm",
+        "transavia",
+        "lidl",
+        "oebb",
+        "wien",
+        "vienna",
+        "graz",
+        "aut",
+        "hiperdino",
+        "supermercado",
+    ],
+    "household": ["hema", "ikea", "jysk", "action"],
+    "entertainment": ["netflix", "spotify", "steam"],
+    "sport": ["decathlon", "intersport"],
+    "drogist": [
+        "kruidvat",
+    ],
+    "car": ["greenwheels"],
+    "loan": ["duo"],
+    "internet": ["youfone"],
+    "invest": ["degiro"],
 }
-
 for keyword, cats in keyword_to_cat.items():
     for i, row in df.iterrows():
         if row["Transactiebedrag"] > 0:
@@ -32,5 +66,12 @@ for keyword, cats in keyword_to_cat.items():
                 df.loc[i, "category"] = keyword
 
 df.to_csv(f"{home}/2023_to_20240130_categorized.csv")
+
+dfsorted = df.sort_values(by=["Transactiebedrag"], ascending=False)
+for i, row in dfsorted.iterrows():
+    if row["Transactiebedrag"] > 0:
+        continue
+    if row["category"] == "other":
+        print(row["Omschrijving"], print(row["Transactiebedrag"]))
 
 print(df.groupby("category")["Transactiebedrag"].sum())
