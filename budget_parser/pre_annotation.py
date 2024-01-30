b2381f76df25b67dcff5b31a8fcd4ad6308e0b70
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-01-30 18:14:18 +0100

    auto commit

--- budget_parser/pre_annotation.py
+++ budget_parser/pre_annotation.py
@@ -1,5 +1,36 @@
+# %%
 import pandas as pd
 import os
 
 home = os.path.expanduser("~")
 df = pd.read_excel(f"{home}/2023_to_20240130.xls")
+df["category"] = ["other"] * len(df)
+description = df["Omschrijving"]
+df = df[df["Transactiebedrag"] < 0]
+
+keyword_to_cat = {
+    "rent": ["vastgoed"],
+    "groceries": ["hoogvliet", "jumbo", "albert", "heijn"],
+    "misc": ["bol.com"],
+    "vacation": ["wien", "vienna", "graz"],
+    "ov": ["ov-chipkaart"],
+    "bike": ["swapfiets"],
+    "insurance": ["fbto"],
+    "healthcare": ["infomedics"],
+    "fun": ["boulder"],
+    "clothes": ["h&m", "zalando"],
+    "phone": ["kpn"],
+    "tax": ["belastingdienst"],
+}
+
+for keyword, cats in keyword_to_cat.items():
+    for i, row in df.iterrows():
+        if row["Transactiebedrag"] > 0:
+            continue
+        for cat in cats:
+            if cat in row.Omschrijving.lower():
+                df.loc[i, "category"] = keyword
+
+df.to_csv(f"{home}/2023_to_20240130_categorized.csv")
+
+print(df.groupby("category")["Transactiebedrag"].sum())
