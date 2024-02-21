c79fa14db479ec51ef52cef24efbebe456c33ba0
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-02-21 12:15:29 +0100

    auto commit

--- visdrone/sequence_to_mm.py
+++ visdrone/sequence_to_mm.py
@@ -41,7 +41,7 @@ for annotations_file,seq in zip(annotation_files,sequences):
 		logstr += f"{timestamp:.2f}\n"
 	with open(logfile,'w') as f:
 		f.write(logstr)
-	mm = MediaManager(videodir,video_suffix='.jpg')
+	mm = MediaManager(videodir,video_suffix='.jpg',result_dirpath=resultsdir)
 	# convert annotations
 	with open(annotations_file,'r') as f:
 		text = f.read()
@@ -80,19 +80,20 @@ for annotations_file,seq in zip(annotation_files,sequences):
 				"timestamp":f"{(int(frame_no)-1)/25:.2f}"
 			}
 			out_data.append(datadict)
+	print(mm.result_dirpath)
 
-	mm.save_annotations(pd.DataFrame(out_data))
-	annotations = mm.load_annotations()
-	from dlutils_ii.tools.drawer import DrawBboxEngine
-	from trackertoolbox.detections import Detections
-	t = mm.timestamps
-	drawer = DrawBboxEngine(color_key='class_id',label_keys=['label'])
-	for i in range(10):
-		frame = mm.get_frame(t[i])
-		annotations_frame = annotations[annotations['timestamp'] == t[i]]
-		f_out = drawer.draw(frame,Detections(annotations_frame))
-		os.makedirs(f'/data/test/',exist_ok=True)
-		cv2.imwrite(f'/data/test/{annotations_file.stem}_test_{i}.jpg',cv2.cvtColor(f_out,cv2.COLOR_RGB2BGR))
+	# mm.save_annotations(pd.DataFrame(out_data))
+	# annotations = mm.load_annotations()
+	# from dlutils_ii.tools.drawer import DrawBboxEngine
+	# from trackertoolbox.detections import Detections
+	# t = mm.timestamps
+	# drawer = DrawBboxEngine(color_key='class_id',label_keys=['label'])
+	# for i in range(10):
+	# 	frame = mm.get_frame(t[i])
+	# 	annotations_frame = annotations[annotations['timestamp'] == t[i]]
+	# 	f_out = drawer.draw(frame,Detections(annotations_frame))
+	# 	os.makedirs(f'/data/test/',exist_ok=True)
+	# 	cv2.imwrite(f'/data/test/{annotations_file.stem}_test_{i}.jpg',cv2.cvtColor(f_out,cv2.COLOR_RGB2BGR))
 
 	
 
@@ -102,7 +103,7 @@ for annotations_file,seq in zip(annotation_files,sequences):
 
 
 
-	# pd.DataFrame(out_data).to_csv(f"{annotations_dir}/{seq.stem}_annotations.csv",index=False)
+	# # pd.DataFrame(out_data).to_csv(f"{annotations_dir}/{seq.stem}_annotations.csv",index=False)
 
  
 
