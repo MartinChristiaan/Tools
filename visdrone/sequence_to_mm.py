5a3716a768d54ea6d189bc5f10d2b9d62003ddb7
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-02-21 11:23:16 +0100

    auto commit

--- visdrone/sequence_to_mm.py
+++ visdrone/sequence_to_mm.py
@@ -2,13 +2,21 @@
 import os
 from pathlib import Path
 import shutil
+
+import pandas as pd
 data = '/data/VisDrone2019-VID-val/VisDrone2019-VID-val/'
 mm_dir=  '/data/visdrone/'
 
 from media_manager.core import MediaManager
 
-sequences = (Path(data)/'sequences').glob('*')
-for seq in sequences:
+sequences = list((Path(data)/'sequences').glob('*'))
+annotation_files = list((Path(data)/'annotations').rglob('*.txt'))
+sequences.sort()
+annotation_files.sort()
+
+
+for annotations_file,seq in zip(annotation_files,sequences):
+	# convert video
 	videodir = Path(f"{mm_dir}/video/{seq.stem}")
 	videodir.mkdir(parents=True,exist_ok=True)
 	resultsdir=  Path(f"{mm_dir}/results/{seq.stem}")
@@ -29,9 +37,58 @@ for seq in sequences:
 		logstr += f"{timestamp}\n"
 	with open(logfile,'w') as f:
 		f.write(logstr)
-	
 	mm = MediaManager(videodir,video_suffix='.jpg')
-	frame = mm.get_frame(1)
+	# convert annotations
+	with open(annotations_file,'r') as f:
+		text = f.read()
+	lines= text.split("\n")
+	out_data = []
+	label_lut = {
+		0:"ignored regions",
+		1:"pedestrian",
+		2:"people",
+		3:"bicycle",
+		4:"car",
+		5:"van",
+		6:"truck",
+		7:"tricycle",
+		8:"awning-tricycle",
+		9:"bus",
+		10:"motor",
+		11:"others"
+	}
+
+	for line in lines:
+		line_split =  line.split(',')
+		if len(line_split) > 1:
+			idx,frame_no,bbox_x,bbox_y,bbox_w,bbox_h,ignore,class_id,_,_ = line.split(',')
+			label = label_lut[int(class_id)]	
+			datadict = {
+				"frame_no":frame_no,
+				"bbox_x":bbox_x,
+				"bbox_y":bbox_y,
+				"bbox_w":bbox_w,
+				"bbox_h":bbox_h,
+				"ignore":ignore,
+				"class_id":class_id,
+				"label":label,
+				"timestamp":int(frame_no)/25.9
+			}
+			out_data.append(datadict)
+	# annotations_dir= Path(f"{resultsdir}/_annotations")
+	# annotations_dir.mkdir(parents=True,exist_ok=True)
+
+	mm.save_annotations(pd.DataFrame(out_data))
+	# pd.DataFrame(out_data).to_csv(f"{annotations_dir}/{seq.stem}_annotations.csv",index=False)
+	
+		
+	
+
+
+
+	
+ 
+	
 
 
 	
