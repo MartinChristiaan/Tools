4d24e532d95185711bdf3b52222378328e29afa8
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-03-18 19:39:44 +0100

    auto commit

--- opencv_annotator/scripts/annotate_drone_detection.py
+++ opencv_annotator/scripts/annotate_drone_detection.py
@@ -16,14 +16,14 @@ datasets =  drone_detection_dataset()
 
 du.Writer.export_multiprocessed(datasets,[0,-15,15],labelconfig=get_sod_label_config())
 #%%
-index =0
+index =7
 # print(datasets[index].pathfinder.name)
 BoundingBoxAnnotator(datasets[index]).run()
 #%%
 x = datasets[index]
 tmp_path = x.pathfinder.annotations_path.with_suffix(".tmp.csv")
 annotations = pd.read_csv(tmp_path)
-	# x.pathfinder.media_manager.save_annotations(annotations, "smallObjectsCorrected", True)
+x.pathfinder.media_manager.save_annotations(annotations, "smallObjectsCorrected", True)
 	# print('uploaded')
 
 
