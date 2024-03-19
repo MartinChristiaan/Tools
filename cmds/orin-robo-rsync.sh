0bcf043f1e2bf1b9031b4a30b7c522ebf6898a3d
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-03-19 14:01:13 +0100

    auto commit

---
+++ cmds/orin-robo-rsync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+while sleep 0.1; do find ~/git  -not -path "*/node_modules/*" | entr -d bash -c "rsync  -azP -e 'ssh -p 22' --delete ~/git/projects//  dev@iilab47.labs.tno.nl:/home/leeuwenmcv/git///"; done
+	
