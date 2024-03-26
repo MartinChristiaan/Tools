849f156ab2e9fba079fe87ad1be2cfcca7906a03
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-03-26 18:15:57 +0100

    auto commit

--- opencv_annotator/opencv_annotator/cache_annotator.py
+++ opencv_annotator/opencv_annotator/cache_annotator.py
@@ -41,22 +41,27 @@ def apply_ignore_areas(annotations: List[Annotation]):
 class IOManager:
     def __init__(self, dataset_configs: List[du.DatasetConfig], state: State) -> None:
         self.state = state
+        self.configs = dataset_configs
         # self.pathfinder = dataset_config.pathfinder
         items = []
+        frame_index = 0
         for i, x in enumerate(dataset_configs):
             annotations = x.pathfinder.annotations()
+            for t, tdf in annotations.groupby("timestamp"):
+                tdf["frame_index"] = [frame_index] * len(tdf)
+                tdf["config_index"] = [i] * len(tdf)
+                items.append(tdf)
+                frame_index += 1
 
             # items += list(enumerate(.groupby("timestamp")))
-
-        self.timestamps = [x[1][0] for x in items]
-        self.detections = [Annotation.from_pandas(x[1]) for x in items]
-
-        # self.dataset_config = dataset_config
+        self.num_frames = frame_index
+        self.detections = [Annotation.from_pandas(x) for x in items]
         self.current_annotations = pd.DataFrame()
-
         if self.tmp_annotation_path.exists():
             self.current_annotations = pd.read_csv(self.tmp_annotation_path)
-        self.state.timestamps.set_value(self.timestamps)
+
+        # self.state.timestamps.set_value(self.timestamps)
+
         self.frame_index = self.get_first_frame_index()
         self.tracked_annotations = []
         self.go_value = ""
@@ -74,10 +79,10 @@ class IOManager:
             if key == "a":
                 frame_index -= 1
                 if frame_index < 0:
-                    frame_index = len(self.timestamps) - 1
+                    frame_index = self.num_frames - 1
             else:
                 frame_index += 1
-                if frame_index > len(self.timestamps) - 1:
+                if frame_index > self.num_frames - 1:
                     frame_index = 0
             self.state.frame_index.set_value(frame_index)
             print("set new index")
@@ -117,20 +122,20 @@ class IOManager:
             ]
 
     @property
-    def evaluated_timestamps(self):
-        return self.current_annotations.timestamp.unique()
+    def evaluated_time_indices(self):
+        return self.current_annotations.frame_index.unique()
 
     def get_first_frame_index(self):
         if len(self.current_annotations) == 0:
             return 0
-        for j in range(len(self.timestamps)):
-            if self.timestamps[j] not in self.evaluated_timestamps:
+        for j in range(self.num_frames):
+            if self.timestamps[j] not in self.evaluated_time_indices:
                 return j
         return 0
 
     @property
     def tmp_annotation_path(self):
-        return self.pathfinder.annotations_path.with_suffix(".tmp.csv")
+        return "tmp_annot.csv"
 
     def save_tmp(self):
         # get current annotations and put them in dataframe
@@ -170,22 +175,24 @@ class IOManager:
     def load_frame(self):
         self.frame_index = self.state.frame_index.value
         # timestamp, detections = self.items[self.frame_index]
-        timestamp = self.timestamps[self.frame_index]
+        # timestamp = self.timestamps[self.frame_index]
         detections = self.detections[self.frame_index]
         if (
             len(self.current_annotations)
-            and timestamp in self.current_annotations.timestamp.unique()
+            and self.frame_index in self.current_annotations.frame_index.unique()
         ):
             detections = Annotation.from_pandas(
                 self.current_annotations[
-                    self.current_annotations.timestamp == timestamp
+                    self.current_annotations.frame_index == self.frame_index
                 ]
             )
         detections += self.tracked_annotations
         detections = apply_ignore_areas(detections)
-        offset = self.dataset_config.options.offset_scales[0]
+        config = self.configs[detections.config_index[0]]
+        timestamp = detections.timestamp[0]
+        offset = config.options.offset_scales[0]
         frames = [
-            cv2.imread(self.dataset_config.pathfinder.frame_filename(o, timestamp))
+            cv2.imread(config.pathfinder.frame_filename(o, timestamp))
             for o in [0, int(round(-15 * offset)), int(round(15 * offset))]
         ]
         vizframe = vizualize_objects(frames)
@@ -206,7 +213,7 @@ class IOManager:
         if len(self.timestamps) < self.frame_index + 2:
             return False
         next_timestamp = self.timestamps[self.frame_index + 1]
-        return not next_timestamp in self.evaluated_timestamps
+        return not next_timestamp in self.evaluated_time_indices
 
     def track(self, annotations: List[Annotation]):
         # TODO use other process for tracking. or as soon as bbox is drawn
