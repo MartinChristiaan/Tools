f1cbf19eaa85fde156ec219e13e690b5205237a8
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-03-31 16:02:41 +0200

    auto commit

---
+++ track_view_server/main.py
@@ -0,0 +1,86 @@
+from flask import Flask, Response, jsonify, request
+from flask_cors import CORS
+import dlutils_ii as du
+from videosets_ii.videosets_ii import VideosetsII
+import cv2
+
+from mm_io_manager import IOData
+
+app = Flask(__name__)
+CORS(app)
+
+
+class VideosetAPI:
+    def __init__(self):
+        self.manager = IOData()
+
+    def get_frame(self, timestamp):
+        if timestamp == 0:
+            timestamp = self.manager.timestamps[0]
+        frame = self.manager.get_frame(timestamp)
+        _, encoded_frame = cv2.imencode(".jpeg", frame)
+        encoded_frame = encoded_frame.tobytes()
+        return Response(encoded_frame, content_type="image/jpeg")
+
+    def get_detections(self, timestamp):
+        if timestamp == 0:
+            timestamp = self.manager.timestamps[0]
+        return self.manager.get_detections(timestamp).to_json(orient="records")
+
+    def get_xt_plot(self):
+        # return self.manager.detections().to_json(orient='records')
+        return dict(
+            x=list(self.manager.detections.timestamp),
+            y=list(self.manager.detections.bbox_x),
+            type="scatter",
+            mode="markers",
+        )
+
+
+videoset_api = VideosetAPI()
+
+
+@app.route("/frame/<timestamp>", methods=["GET"])
+def get_frame(timestamp):
+    print(timestamp)
+    return videoset_api.get_frame(float(timestamp))
+
+
+@app.route("/detections/<timestamp>", methods=["GET"])
+def get_detections(timestamp):
+    return videoset_api.get_detections(float(timestamp))
+
+
+@app.route("/plotdata", methods=["GET"])
+def get_xt_plot():
+    return videoset_api.get_xt_plot()
+
+
+@app.route("/videoset", methods=["GET"])
+def get_videoset():
+    return jsonify(videoset_api.manager.to_dict())
+
+
+@app.route("/videoset", methods=["POST"])
+def set_videoset():
+    data = request.json  # JSON data sent in the request
+    videoset_api.manager.set_videoset_data(data)
+    return jsonify(videoset_api.manager.to_dict())
+
+
+@app.route("/handle_key/<key>", methods=["GET"])
+def next_frame(key):
+    if key in "ad":
+        delta = 1
+        if key == "a":
+            delta = -1
+        videoset_api.frame_index += delta
+
+    # if key in 'xz':
+    # 	videoset_api.update_frame_variation(-1 if key == 'z' else 1)
+
+    return "{}"  # videoset_api.get_annotations()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ track_view_server/mm_io_manager.py
@@ -0,0 +1,92 @@
+import os
+from pathlib import Path
+from loguru import logger
+
+from videosets_ii.videosets_ii import VideosetsII
+from trackertoolbox.detections import Detections
+from trackertoolbox.tracks import Tracks, TrackUpdates
+import pandas as pd
+
+basedirpath = Path(r"/diskstation")
+videosets = VideosetsII(basedirpath=basedirpath)  # basedirpath)
+names = list(videosets.to_pandas()["name"])
+
+
+def get_modified_date(path):
+    return os.path.getmtime(path)
+
+
+class IOData:
+    def __init__(self) -> None:
+        self.videoset = "aot"
+        self.camera = "part1/video0360"
+
+        self.cameras = videosets[self.videoset].cameras
+        # self.detections_sources = ["tracks/yolov8s_precision_20240301_resnet18.csv"]
+        self.mm = videosets[self.videoset].get_mediamanager(self.camera)
+        self.detections_sources = self.load_annotation_source(self.mm)
+        self.selected_sources = self.detections_sources[:1]
+        self.detections = self.load_detections()
+
+    def to_dict(self):
+        return dict(
+            videoset=self.videoset,
+            camera=self.camera,
+            cameras=self.cameras,
+            videosets=names,
+            sources=[str(x) for x in self.detections_sources],
+            selected_source=[str(x) for x in self.selected_sources],
+        )
+
+    def set_videoset_data(self, data):
+        print(data)
+        if self.videoset != data["videoset"]:
+            self.videoset = data["videoset"]
+            self.cameras = videosets[self.videoset].cameras
+            self.camera = self.cameras[0]
+            self.update_mm()
+
+        elif self.camera != data["camera"]:
+            self.camera = self.cameras[0]
+            self.update_mm()
+
+        elif self.selected_sources == data["selected_sources"]:
+            self.detections = self.load_detections()
+
+    def update_mm(self):
+        self.mm = videosets[self.videoset].get_mediamanager(self.camera)
+        self.detections_sources = self.load_annotation_source(self.mm)
+        self.selected_sources = [
+            source
+            for source in self.selected_sources
+            if source in self.detections_sources
+        ]
+        self.detections = self.load_detections()
+
+    @property
+    def timestamps(self):
+        return self.mm.timestamps
+
+    def get_frame(self, timestamp):
+        return self.mm.get_frame(timestamp)
+
+    def get_detections(self, timestamp):
+        return self.detections[self.detections.timestamp == timestamp]
+
+    def load_detections(self):
+        detections_list = []
+        for source in self.selected_sources:
+            path = f"{source.parent.stem}/{source.name}"
+            from_supra = "_supra" in path
+            detections = self.mm.load(path.replace("_supra/", ""), from_supra)
+            detections["source"] = source
+            detections_list.append(detections)
+        if len(detections_list) > 0:
+            return pd.concat(detections_list)
+        return None
+
+    def load_annotation_source(self, mm):
+        paths = list(mm.result_dirpath.rglob("*.csv"))
+        sorted_paths = sorted(paths, key=get_modified_date)
+        latest_path = [x for x in sorted_paths]
+        return latest_path
