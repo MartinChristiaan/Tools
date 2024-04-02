035361f076e73c0777f2f489e2e4155da3b4963b
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-02 14:04:51 +0200

    auto commit

--- zoomer/imagezoom.py
+++ zoomer/imagezoom.py
@@ -1,7 +1,31 @@
+#%%
 import numpy as np
 import cv2
 
-image = cv2.imread('/data/sod_cache/raw/drone-tracking/cam0_video_cam0_video/0/48631.jpg')
+# image = cv2.imread('/data/sod_cache/raw/drone-tracking/cam0_video_cam0_video/0/48631.jpg')
+array = np.arange(2000)
+
+
+zoom = 1
+x = 200
+y = 200
+
+zoommult = 1.1
+h,w=  image.shape[:2]
+
+for zoomindex in range(8):
+	zoom = zoom * zoommult
+
+
+
+
+
+
+
+
+
+
+
 
 
 
