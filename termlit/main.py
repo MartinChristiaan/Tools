c079f3cdf3824266c32c12d7c9c575d9b07d7096
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-19 19:44:29 +0200

    auto commit

--- termlit/main.py
+++ termlit/main.py
@@ -1,21 +1,36 @@
+# import os
+# from pathlib import Path
+# from loguru import logger
 
-import os
-from pathlib import Path
+# from videosets_ii.videosets_ii import VideosetsII
+# from trackertoolbox.detections import Detections
+# from trackertoolbox.tracks import Tracks,TrackUpdates
+# import pandas as pd
+
+# basedirpath = Path(r"/diskstation")
+# videosets = VideosetsII(basedirpath= basedirpath)#basedirpath)
+
+
+from typing import List
+import click
+
+import fnmatch
 from loguru import logger
 
-from videosets_ii.videosets_ii import VideosetsII
-from trackertoolbox.detections import Detections
-from trackertoolbox.tracks import Tracks,TrackUpdates
-import pandas as pd
 
-basedirpath = Path(r"/diskstation")
-videosets = VideosetsII(basedirpath= basedirpath)#basedirpath)
+def multi_select(options: List[str]):
+	if len(options) == 0:
+		logger.warning('no options available')
+		return None
+	current_pattern = ""
+	while True:
+		selected = []
+		for sub_pattern in current_pattern.split("+"):
+			selected += fnmatch.filter(options, f"*{sub_pattern}*")
 
-
-
-
-
-
-
+		print(current_pattern + " : ")
+		print(",".join(selected))
+		char = click.getchar()
+		if char == 'space':
 
 
