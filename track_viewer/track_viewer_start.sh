cfaf8825d32e17bdb785ce0e47f25098b2cf0e49
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-19 11:27:12 +0200

    auto commit

--- track_viewer/track_viewer_start.sh
+++ track_viewer/track_viewer_start.sh
@@ -1,2 +1,2 @@
-ssh leeuwenmcv@pc-11393.tsn.tno.nl -p 10041 "cd ~/git/tools/track_viewer && make run"
+ssh leeuwenmcv@pc-11393.tsn.tno.nl -p 10041 "cd ~/git/tools/track_viewer && make up"
 xdg-open http://pc-11393.tsn.tno.nl:14118/
