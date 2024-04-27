3ca92609b79aede77bb771d1e9329f011d1a2474
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-27 19:11:01 +0200

    auto commit

--- smartlit/MediaManagerSelection.py
+++ smartlit/MediaManagerSelection.py
@@ -8,6 +8,8 @@ from trackertoolbox.tracks import Tracks, TrackUpdates
 from pathlib import Path
 import pandas as pd
 
+from state import Observable
+
 basedirpath = Path(r"/diskstation")
 videosets = VideosetsII(basedirpath=basedirpath)  # basedirpath)
 default_vset = "drone_detection_dataset_2021"
@@ -29,9 +31,52 @@ def get_modified_date(path):
     return os.path.getmtime(path)
 
 
-class PlotData:
+class PlotData(Observable):
     def __init__(self, df, x_axis_label, y_axis_label, pivot_column) -> None:
-        pass
+        self.value = df
+        self.x_axis_label = x_axis_label
+        self.y_axis_label = y_axis_label
+        self.pivot_column = pivot_column
+        self.plot_type = "scatter"
+
+    def get_ui_data(self):
+        data = self.value
+        if len(data) == 0:
+            return {}
+
+        if self.plot_type == "scatter":
+            datadict = dict(
+                x=list(data.timestamp),
+                y=list(data.bbox_x),
+                type="scatter",
+                mode=self.plotmode,
+                name="bbox_x",
+            )
+            if self.color != "":
+                datadict.update(
+                    {
+                        "marker": {
+                            "color": list(data[self.color]),
+                            "colorscale": "Jet",
+                        }
+                    }
+                )
+            return [datadict]
+
+        else:
+            datas = []
+            for groups, groupdf in data.groupby(self.pivot_column):
+                datadict = dict(
+                    x=list(groupdf.timestamp),
+                    y=list(groupdf.bbox_x),
+                    type="scatter",
+                    mode=self.plotmode,
+                    name=",".join([str(x) for x in groups]),
+                )
+                # if self.color != "":
+                #     datadict.update({"color": list(data[self.color])})
+                datas.append(datadict)
+            return datas
 
 
 class MediaManagerSelection(Container):
@@ -45,14 +90,13 @@ class MediaManagerSelection(Container):
         self.camera = SelectBoxObservable(
             default_cam, "camera", uimode="selectbox", options=default_cams
         )
-        self.data_table = SelectBoxObservable("", options=[])
-
         self.videosets = videosets
         self.mm = videosets[self.videoset.value].get_mediamanager(self.camera.value)
 
         self.videoset.subscribe(self.on_videoset_update)
         self.camera.subscribe(self.on_camera_update)
-
+        self.data_table_path = SelectBoxObservable("", options=[])
+        self.data_table = Observable(pd.DataFrame(), "data_table")
         super().__init__("Media Manager Selection")
 
     def on_videoset_update(self):
