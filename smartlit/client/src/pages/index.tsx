6c34ea57cfd6660651b83cff63099d12bbf1310a
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-27 10:23:53 +0200

    auto commit

--- smartlit/client/src/pages/index.tsx
+++ smartlit/client/src/pages/index.tsx
@@ -1,63 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 
-const Chart = dynamic(
-  () => {
-    return import("react-apexcharts");
-  },
-  { ssr: false }
-);
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      options: {
-        chart: {
-          id: "basic-line",
-          events: {
-            click: (event, chartContext, config) => {
-              // console.log(event, chartContext, config);
-              console.log(config.DataPointIndex)
-              console.log(config)
+const Home = () => {
+  const xdata=[1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+  const [series, setSeries] = useState([
+    {
+      name: "data1",
+      data: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    },
+  ]);
+  const [options, setOptions] = useState({
+    chart: {
+      id: "basic-line",
+      events: {
+        click: (event, chartContext, config) => {
+          const clickedX = xdata[config.dataPointIndex];
+          const updatedOptions = {
+            ...options,
+            annotations: {
+              xaxis: [
+                {
+                  x: clickedX,
+                  borderColor: "#000",
+                },
+              ],
             },
-          },
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+          };
+          setOptions(updatedOptions);
         },
       },
-      series: [
-        {
-          name: "data1",
-          data: [0,1,2,3,4,5,6,7,8]        
-        },
-      ],
-    };
-  }
+    },
+    xaxis: {
+      categories: xdata,
+    },
+  });
 
-  onclick(c) {
-    console.log(c);
-  }
 
-  render() {
-    return (
-      <div className="app">
-        <div className="row">
-          <div className="line-chart">
-            <Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="line"
-              width="500"
-            />
-          </div>
+
+  return (
+    <div className="app">
+      <div className="row">
+        <div className="line-chart">
+          <Chart options={options} series={series} type="line" width="500" />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
-
