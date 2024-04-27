4d14cad11f6312c748b937b57b15407161e0e49c
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-27 19:00:55 +0200

    auto commit

--- smartlit/MediaManagerSelection.py
+++ smartlit/MediaManagerSelection.py
@@ -15,6 +15,25 @@ default_cams = videosets[default_vset].cameras
 default_cam = videosets[default_vset].cameras[5]
 
 
+def find_result_csv_in_mm_path(mm):
+    paths = list(mm.result_dirpath.rglob("*.csv"))
+    sorted_paths = sorted(paths, key=get_modified_date)
+    # path_options = [f"{x.parent.stem}/{x.name}" for x in sorted_paths]
+    return sorted_paths
+
+
+import os
+
+
+def get_modified_date(path):
+    return os.path.getmtime(path)
+
+
+class PlotData:
+    def __init__(self, df, x_axis_label, y_axis_label, pivot_column) -> None:
+        pass
+
+
 class MediaManagerSelection(Container):
     def __init__(self) -> None:
         self.videoset = SelectBoxObservable(
@@ -26,11 +45,24 @@ class MediaManagerSelection(Container):
         self.camera = SelectBoxObservable(
             default_cam, "camera", uimode="selectbox", options=default_cams
         )
-        self.videoset.subscribe(self.on_videoset_update)
+        self.data_table = SelectBoxObservable("", options=[])
+
         self.videosets = videosets
+        self.mm = videosets[self.videoset.value].get_mediamanager(self.camera.value)
+
+        self.videoset.subscribe(self.on_videoset_update)
+        self.camera.subscribe(self.on_camera_update)
+
         super().__init__("Media Manager Selection")
 
     def on_videoset_update(self):
         cur_vset = self.videoset.value
         self.camera.options = self.videosets[cur_vset].cameras
         self.camera.set_value(self.camera.options[0])
+
+    def on_camera_update(self):
+        self.mm = videosets[self.videoset.value].get_mediamanager(self.camera.value)
+        self.data_table.options = find_result_csv_in_mm_path(self.mm)
+
+    def on_data_table_update(self):
+        df = self.mm.load(self.data_table.value)
