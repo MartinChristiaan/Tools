3b7918f9e98ee5ddd1f972bec809c9805d845a50
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-27 10:03:39 +0200

    auto commit

--- smartlit/client/src/pages/index.tsx
+++ smartlit/client/src/pages/index.tsx
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import Chart from "react-apexcharts";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(
+  () => {
+    return import("react-apexcharts");
+  },
+  { ssr: false }
+);
 
 class Home extends Component {
   constructor(props) {
@@ -8,30 +15,39 @@ class Home extends Component {
     this.state = {
       options: {
         chart: {
-          id: "basic-bar"
+          id: "basic-scatter",
+          events: {
+            click: (event, chartContext, config) => {
+              console.log(event, chartContext, config);
+            },
+          },
         },
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-        }
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
       },
       series: [
         {
-          name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }
-      ]
+          name: "data1",
+          data: [0,1,2,3,4,5,6,7,8]        
+        },
+      ],
     };
+  }
+
+  onclick(c) {
+    console.log(c);
   }
 
   render() {
     return (
       <div className="app">
         <div className="row">
-          <div className="mixed-chart">
+          <div className="scatter-chart">
             <Chart
               options={this.state.options}
               series={this.state.series}
-              type="bar"
+              type="scatter"
               width="500"
             />
           </div>
@@ -42,3 +58,4 @@ class Home extends Component {
 }
 
 export default Home;
+
