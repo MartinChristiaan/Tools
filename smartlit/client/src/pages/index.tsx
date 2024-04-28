bab4d9ef48386e0f6455818674e4e73a4615f1f9
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-28 13:54:57 +0200

    auto commit

--- smartlit/client/src/pages/index.tsx
+++ smartlit/client/src/pages/index.tsx
@@ -1,41 +1,79 @@
-import { Card, CardHeader, CardBody, CardFooter,Heading,StackDivider,Stack,Box,Text} from '@chakra-ui/react';
+
+import { Card, Box, Heading, Form } from "@chakra-ui/react";
+
+import { useEffect, useState } from "react";
+import { flask_url } from "../lib/utils";
+import { render_selectbox } from "../lib/selectbox";
+
 
 export default function Home() {
-  return (
-<Card>
-  <CardHeader>
-    <Heading size='md'>Client Report</Heading>
-  </CardHeader>
+  const [uiData, setUiData] = useState(null);
 
-  <CardBody>
-    <Stack divider={<StackDivider />} spacing='4'>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'>
-          Summary
+  useEffect(() => {
+    fetch(flask_url+'/get_ui_data')
+      .then(response => response.json())
+      .then(data => setUiData(data));
+  }, []);
+
+
+  function onUpdate(cardKey, innerKey, value) {
+    // Make a copy of uiData
+    let newUiData = { ...uiData };
+    // Update the value in the local state
+    newUiData[cardKey][innerKey].value = value;
+
+    // Perform the POST request to update the server
+    fetch(flask_url + "/set_ui_data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUiData),
+    })
+      .then(response => response.json())
+      .then((data) => {
+        setUiData(data);
+      })
+      .catch((error) => {
+        console.error("Error updating UI data:", error);
+      });
+  }
+
+
+  const renderFormElement = (outerKey,innerkey,data) => {
+    const datastr = data.toString()
+    switch (data.uimode) {
+      case "selectbox":
+        return render_selectbox(innerkey, data, onUpdate, outerKey);
+      default:
+        return null;
+    }
+  };
+
+
+const renderCard = (key, innerData) => {
+  return (
+    <Card key={key} my={3}>
+      <Box p={4}>
+        <Heading as="h2" size="md" mb={4}>
+          {key}
         </Heading>
-        <Text pt='2' fontSize='sm'>
-          View a summary of all your clients over the last month.
-        </Text>
+          {Object.entries(innerData).map(([innerKey, innerData]) =>
+            renderFormElement(key, innerKey, innerData)
+          )}
       </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'>
-          Overview
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          Check out the overview of your clients.
-        </Text>
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'>
-          Analysis
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          See a detailed analysis of all your business clients.
-        </Text>
-      </Box>
-    </Stack>
-  </CardBody>
-</Card>
+    </Card>
+  );
+};
+
+
+  return (
+    <div>
+      {uiData &&
+        Object.entries(uiData).map(([outerKey, innerData]) =>
+          renderCard(outerKey, innerData)
+        )}
+    </div>
   );
 }
 
