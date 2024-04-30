5a75843bd3eb405696bcb359bdfe7f861cc8fb05
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-30 16:45:46 +0200

    auto commit

--- track_viewer/client/src/components/data_selection.tsx
+++ track_viewer/client/src/components/data_selection.tsx
@@ -2,11 +2,42 @@
 
 import { Videoset,defaultVideoset,VideosetOption, fetchVideosetOptions } from "@/lib/ServerData";
 import { flask_url } from "@/lib/utils";
+import { FormControl, FormLabel } from "@chakra-ui/react";
+import { AutoComplete, AutoCompleteInput, AutoCompleteItem, AutoCompleteList } from "@choc-ui/chakra-autocomplete";
 import { use, useEffect,useState } from "react";
+
+
+function get_autocomplete_box(label:string,value:string,options:string[],onSelect:any){
+	return (
+	<FormControl w="200">
+        <FormLabel>{label}</FormLabel>
+        <AutoComplete openOnFocus onSelectOption={(x) => onSelect(x.item.value)}>
+          <AutoCompleteInput variant="filled" placeholder={value} />
+          <AutoCompleteList>
+            {options.map((options, cid) => (
+              <AutoCompleteItem
+                key={`option-${cid}`}
+                value={options}
+              >
+                {options}
+              </AutoCompleteItem>
+            ))}
+          </AutoCompleteList>
+        </AutoComplete>
+      </FormControl>
+	)
+}
+
+
 export default function VideosetSelector({videoset,SetVideoset}:{videoset:Videoset,SetVideoset:any}){
 	const [videosetOptions,setVideosetOptions] = useState<VideosetOption[]>([]);
 	useEffect(() => {
 		fetchVideosetOptions(setVideosetOptions)
 	}, []);
-	return <></>
+
+	const videoset_names = videosetOptions.map(videoset=>videoset.videoset)
+
+	return <>
+		{get_autocomplete_box('Videoset',videoset.name,videoset_names,(value:string)=>{SetVideoset({...videoset,name:value})})}
+	</>
 }
--- track_viewer/client/src/pages/index.tsx
+++ track_viewer/client/src/pages/index.tsx
@@ -1,9 +1,17 @@
-import BboxDrawer from "../components/bbox_drawer";
-import TemporalPlot  from "../components/plot";
+// import BboxDrawer from "../components/bbox_drawer";
+// import TemporalPlot  from "../components/plot";
 import { useEffect, useState } from "react";
-import InputSelector from "../components/input_selector";
-import { flask_url } from "../lib/utils";
+// import InputSelector from "../components/input_selector";
+// import { flask_url } from "../lib/utils";
+import { Videoset,defaultVideoset } from "@/lib/ServerData";
+import VideosetSelector from "@/components/data_selection";
+
 
 export default function Home() {
-  return <></>
+  const [videoset, setVideoset] = useState<Videoset>(defaultVideoset);
+
+
+  return <>
+    <VideosetSelector videoset={videoset} SetVideoset={setVideoset}/>
+  </>
 }
