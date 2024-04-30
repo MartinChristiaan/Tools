fc8474b05d98138f710728997e83317e28ee6a92
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-30 16:35:40 +0200

    auto commit

--- track_viewer/client/src/components/data_selection.tsx
+++ track_viewer/client/src/components/data_selection.tsx
@@ -1,5 +1,12 @@
 //# uses chackra autocomplete to select videoset camera and detection source
-export default function InputSelector(){
 
-
+import { Videoset,defaultVideoset,VideosetOption, fetchVideosetOptions } from "@/lib/ServerData";
+import { flask_url } from "@/lib/utils";
+import { use, useEffect,useState } from "react";
+export default function VideosetSelector({videoset,SetVideoset}:{videoset:Videoset,SetVideoset:any}){
+	const [videosetOptions,setVideosetOptions] = useState<VideosetOption[]>([]);
+	useEffect(() => {
+		fetchVideosetOptions(setVideosetOptions)
+	}, []);
+	return <></>
 }
