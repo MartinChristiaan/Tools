031dd8bd3d49e9d200626733efcb68bedd6035de
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-04-30 16:55:53 +0200

    auto commit

--- track_viewer/client/src/components/data_selection.tsx
+++ track_viewer/client/src/components/data_selection.tsx
@@ -29,15 +29,45 @@ function get_autocomplete_box(label:string,value:string,options:string[],onSelec
 }
 
 
+// @app.route("/detections_options", methods=["GET"])
+// def get_detections_options():
+//     videoset = request.args.get("videoset")
+//     camera = request.args.get("camera")
+
+//     options = videoset_api.get_detections_options(videoset, camera)
+//     return jsonify(options)
+
+
 export default function VideosetSelector({videoset,SetVideoset}:{videoset:Videoset,SetVideoset:any}){
 	const [videosetOptions,setVideosetOptions] = useState<VideosetOption[]>([]);
+	const [detectionOptions,setDetectionOptions] = useState<string[]>([]);
+
+	function fetch_detections_options(){
+		fetch(flask_url+'/detections_options?videoset='+videoset.name+'&camera='+videoset.camera).then(
+			response=>response.json()
+		).then(
+			data=>setDetectionOptions(data)
+		)
+	}
 	useEffect(() => {
 		fetchVideosetOptions(setVideosetOptions)
 	}, []);
 
+
+	useEffect(() => {
+		fetch_detections_options()
+	}, [videoset]);
+
+
 	const videoset_names = videosetOptions.map(videoset=>videoset.videoset)
+	const cameras = videosetOptions.find(x=>x.videoset==videoset.name)?.cameras || []
+
+
+
+
 
 	return <>
 		{get_autocomplete_box('Videoset',videoset.name,videoset_names,(value:string)=>{SetVideoset({...videoset,name:value})})}
+		{get_autocomplete_box('Camera',videoset.camera,cameras,(value:string)=>{SetVideoset({...videoset,camera:value})})}
 	</>
 }
