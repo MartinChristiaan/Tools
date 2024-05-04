b32bb5719e28bf3a5650a18ae072106fc2d7595c
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-05-04 17:09:34 +0200

    auto commit

--- opencv_summary_viewer/main.py
+++ opencv_summary_viewer/main.py
@@ -10,18 +10,41 @@ summaries = (
     "/mnt/dl-41/data/leeuwenmcv/general/ablation_tyolo/proposed-20240326/summaries"
 )
 summaries = Path(summaries).rglob("*.webm")
+import os
+from pathlib import Path
+from loguru import logger
+
+from videosets_ii.videosets_ii import VideosetsII
+import pandas as pd
+
+basedirpath = Path(r"/diskstation")
+videosets = VideosetsII(basedirpath=basedirpath)  # basedirpath)
+names = list(videosets.to_pandas()["name"])
+mantis = [x for x in names if "mantis" in x]
+
 
 # %% Read summaries with opencv and show content to the user
-
-
-def mouse_callback(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print("Clicked at coordinates:", x, y)
 
 
 next_video = False
 should_exit = False
 for summary in summaries:
+
+    videoset_name = [x for x in names if x in str(summary.stem)][0]
+    cameras = videosets[videoset_name].cameras
+    camera_str = str(summary.parent.stem).replace(videoset_name + "_", "")
+    camera = [x for x in cameras if x.replace("/", "_") == camera_str][0]
+
+    def mouse_callback(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            # print("Clicked at coordinates:", x, y)
+            x_idx = x // 200
+            y_idx = y // 200
+            idx = x_idx + y_idx * 6
+            # track_id = summary.stem
+            track_ids = summary.stem.replace(videoset_name, "").replace(camera_str, "")
+            print(track_ids)
+
     if should_exit:
         break
     next_video = False
