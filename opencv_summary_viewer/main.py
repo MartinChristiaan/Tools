5a19e03dcb377b9d45c55b758c3194737ee27907
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-05-05 11:00:32 +0200

    auto commit

--- opencv_summary_viewer/main.py
+++ opencv_summary_viewer/main.py
@@ -1,6 +1,8 @@
 # %%
 #
+from math import pi
 from pathlib import Path
+import pickle
 import time
 import cv2
 import numpy as np
@@ -9,7 +11,7 @@ import numpy as np
 summaries = (
     "/mnt/dl-41/data/leeuwenmcv/general/ablation_tyolo/proposed-20240326/summaries"
 )
-summaries = Path(summaries).rglob("*.webm")
+summaries = Path(summaries).rglob("*.pkl")
 import os
 from pathlib import Path
 from loguru import logger
@@ -25,15 +27,15 @@ mantis = [x for x in names if "mantis" in x]
 
 # %% Read summaries with opencv and show content to the user
 
-
 next_video = False
 should_exit = False
-for summary in summaries:
 
-    videoset_name = [x for x in names if x in str(summary.stem)][0]
-    cameras = videosets[videoset_name].cameras
-    camera_str = str(summary.stem).replace(videoset_name + "_", "")
-    camera = [x for x in cameras if x.replace("/", "_") == camera_str][0]
+
+for summary in summaries:
+    metadata = pickle.load(open(summary, "rb"))
+    summary_video = summary.with_suffix(".webm")
+    name = f"{metadata['videoset']}_{metadata['camera'].replace('/', '_')}"
+    track_ids = summary.stem.replace(name, "")[1:].split("_")
 
     def mouse_callback(event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -41,16 +43,30 @@ for summary in summaries:
             x_idx = x // 200
             y_idx = y // 200
             idx = x_idx + y_idx * 6
-            # track_id = summary.stem
-            track_ids = summary.stem.replace(videoset_name, "").replace(camera_str, "")
-            print(track_ids)
+            if idx < len(track_ids):
+                track_id = track_ids[idx]
+                data = dict(
+                    videoset=metadata["videoset"],
+                    camera=metadata["camera"],
+                    track_id=track_id,
+                    comment="",
+                )
+                print(data)
+                interesting_path = Path("interesting_moments.csv")
+
+                pd.DataFrame([data]).to_csv(
+                    interesting_path,
+                    header=not interesting_path.exists(),
+                    mode="a",
+                    index=False,
+                )
 
     if should_exit:
         break
     next_video = False
     while not next_video and not should_exit:
-        print("opening video", summary)
-        cap = cv2.VideoCapture(str(summary))
+        print("opening video", summary.stem)
+        cap = cv2.VideoCapture(str(summary_video))
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
