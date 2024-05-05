b3d524e862c634c85718e27b2c84acea23f4202f
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-05-05 21:13:22 +0200

    auto commit

--- opencv_summary_viewer/main.py
+++ opencv_summary_viewer/main.py
@@ -11,7 +11,7 @@ import numpy as np
 summaries = (
     "/mnt/dl-41/data/leeuwenmcv/general/ablation_tyolo/proposed-20240326/summaries"
 )
-summaries = Path(summaries).rglob("*.pkl")
+summaries = list(Path(summaries).rglob("*.pkl"))
 import os
 from pathlib import Path
 from loguru import logger
@@ -30,48 +30,57 @@ mantis = [x for x in names if "mantis" in x]
 next_video = False
 should_exit = False
 
+video_idx = 20
 
-for summary in list(summaries)[::-1]:
+# for summary in list(summaries)[::-1]:
+while True:
+    summary = summaries[video_idx]
     metadata = pickle.load(open(summary, "rb"))
     summary_video = summary.with_suffix(".webm")
     name = f"{metadata['videoset']}_{metadata['camera'].replace('/', '_')}"
     track_ids = summary.stem.replace(name, "")[1:].split("_")
 
-    def mouse_callback(event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            # print("Clicked at coordinates:", x, y)
-            x_idx = x // 200
-            y_idx = y // 200
-            idx = x_idx + y_idx * 6
-            if idx < len(track_ids):
-                track_id = track_ids[idx]
-                data = dict(
-                    videoset=metadata["videoset"],
-                    camera=metadata["camera"],
-                    track_id=track_id,
-                    comment="",
-                )
-                print(data)
-                interesting_path = Path("interesting_moments.csv")
-
-                pd.DataFrame([data]).to_csv(
-                    interesting_path,
-                    header=not interesting_path.exists(),
-                    mode="a",
-                    index=False,
-                )
-
     if should_exit:
         break
     next_video = False
     while not next_video and not should_exit:
-        print("opening video", summary.stem)
+        print("opening video", summary.stem, video_idx)
         cap = cv2.VideoCapture(str(summary_video))
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 ret, frame = cap.read()
+            # print(frame.shape)
+            if frame is None:
+                next_video = True
+                continue
+
+            def mouse_callback(event, x, y, flags, param):
+                if event == cv2.EVENT_LBUTTONDOWN:
+                    # print("Clicked at coordinates:", x, y)
+                    h, w = frame.shape[:2]
+                    rows = w // 200
+                    x_idx = x // 200
+                    y_idx = y // 200
+                    idx = x_idx + y_idx * rows
+                    if idx < len(track_ids):
+                        track_id = track_ids[idx]
+                        data = dict(
+                            videoset=metadata["videoset"],
+                            camera=metadata["camera"],
+                            track_id=track_id,
+                            comment="",
+                        )
+                        print(data)
+                        interesting_path = Path("interesting_moments.csv")
+
+                        pd.DataFrame([data]).to_csv(
+                            interesting_path,
+                            header=not interesting_path.exists(),
+                            mode="a",
+                            index=False,
+                        )
 
             cv2.imshow("summary", frame)
             cv2.setMouseCallback("summary", mouse_callback)
@@ -79,6 +88,7 @@ for summary in list(summaries)[::-1]:
 
             if k == ord("n"):
                 next_video = True
+                video_idx += 1
                 break
             if k == ord("q"):
                 should_exit = True
