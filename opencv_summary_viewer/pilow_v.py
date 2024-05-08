70c036576a05bdd1d5ac7bc1c0752396586deb97
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-05-08 15:42:59 +0200

    auto commit

--- opencv_summary_viewer/pilow_v.py
+++ opencv_summary_viewer/pilow_v.py
@@ -14,6 +14,20 @@ def add_text_to_image(image_array, text, font_path, font_size, position, color):
     # Define the font style and size
     font = ImageFont.truetype(font_path, size=font_size)
 
+    # Get the text size
+
+    text_width = max([draw.textlength(x, font=font) for x in text.split("\n")])
+    text_height = font_size * text.count("\n") + font_size
+
+    # Calculate the background rectangle coordinates
+    bg_left = position[0]
+    bg_top = position[1]
+    bg_right = position[0] + text_width
+    bg_bottom = position[1] + text_height
+
+    # Draw the background rectangle
+    draw.rectangle([(bg_left, bg_top), (bg_right, bg_bottom)], fill=(0, 0, 0))
+
     # Write the text on the image
     draw.text(position, text, font=font, fill=color)
 
