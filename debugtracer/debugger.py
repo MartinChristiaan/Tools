7e4e62503dcdddc7cdf3189ae8839ef80163fd67
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-05-12 11:20:16 +0200

    auto commit

--- debugtracer/debugger.py
+++ debugtracer/debugger.py
@@ -1,20 +1,64 @@
 import pickle
+from attr import dataclass
 from click import getchar
 from pathlib import Path
 
-previous_state_path = Path("/data/trace_data/previous_state.pkl")
-state = None
-if previous_state_path.exists():
-    with open(previous_state_path, "rb") as f:
-        state = pickle.load(f)
 from fzf_utils import prompt
 
-if state is None:
-    script_options = list(map(Path("/data/trace_data").glob("*")), str)
-    script = prompt(script_options, False, "Select script")
-    function_options = list(map(script.glob("*")), str)
-    function = prompt(function_options, False, "Select function")
-    iteration = prompt(range(10), False, "Select iteration")
-	state = {"script": script, "function": function, "iteration": iteration}
+previous_state_path = Path("/data/trace_data/previous_state.pkl")
 
 
+def load_previous_state():
+    if previous_state_path.exists():
+        with open(previous_state_path, "rb") as f:
+            debugger = pickle.load(f)
+    else:
+        debugger = Debugger()
+    return debugger
+
+
+class Debugger:
+    def __init__(self, script=None, function=None, iteration=0) -> None:
+        self.script = script
+        self.function = function
+        self.iteration = iteration
+        if self.script is None:
+            self.set_script()
+        if self.function is None:
+            self.set_function()
+
+    def set_script(self):
+        script_options = list(map(str, Path("/data/trace_data").glob("*")))
+        self.script = Path(prompt(script_options, False, "Select script"))
+        self.set_function()
+
+    def set_function(self):
+        function_options = list(map(str, self.script.glob("*")))
+        self.function = Path(prompt(function_options, False, "Select function"))
+        self.set_iteration()
+
+    def set_iteration(self):
+        self.iteration = int(input("Enter iteration: "))
+        self.save()
+
+    def save(self):
+        with open(previous_state_path, "wb") as f:
+            pickle.dump(self, f)
+
+    def run(self):
+        ui_str = f"""
+current state:
+script: {self.script.stem}
+function: {self.function.stem}
+iteration: {self.iteration}
+---------------------------------------------
+s : select script
+f : select function
+i : select iteration	
+r : run function
+"""
+        print(ui_str)
+
+
+if __name__ == "__main__":
+    debugger = load_previous_state()
