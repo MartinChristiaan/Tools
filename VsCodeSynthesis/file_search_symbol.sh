4b768fc7fa22519f453753a49bb0e38ac1094e98
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-05-20 13:32:43 +0200

    auto commit

--- VsCodeSynthesis/file_search_symbol.sh
+++ VsCodeSynthesis/file_search_symbol.sh
@@ -1,4 +1,3 @@
 FILEPATH=$1
-TARGET=$(rg -g '*.py' -i  -p  '(def |class )' $FILEPATH --with-filename --line-buffered --no-heading |fzf --ansi | awk -F ':' '{print $1 ":" $2}')
+TARGET=$(rg -g '*.py' -i  -p  '(def |class )' $FILEPATH  --line-buffered --no-heading |fzf --ansi | awk -F ':' '{print $FILE ":" $2}')
 code -g $TARGET
-pen
