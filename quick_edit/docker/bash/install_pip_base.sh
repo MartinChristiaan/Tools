f81ba9182732ad1764f86651d26fa13184dc6a5b
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-06-07 10:06:01 +0200

    auto commit

---
+++ quick_edit/docker/bash/install_pip_base.sh
@@ -0,0 +1,10 @@
+pip3 install loguru \
+	numpy \
+	pandas \
+	wget \
+	streamlit \
+	jupyterlab \
+	git+https://dlutils_ii:glpat-EQCGEC3FjaXctPnr6jQt@gitlab.tsn.tno.nl/intelligent_imaging/python/deep_learning/dlutils_ii.git \
+	viztracer \
+	opencv-python \
+	click
