80bb3ae74ed07d26f274ff62162b63fb14459e8c
leeuwenmcv <martin.vanleeuwen@tno.nl>
2024-06-17 16:24:45 +0200

    auto commit

--- opencv_summary_viewer/analyze_tracks.py
+++ opencv_summary_viewer/analyze_tracks.py
@@ -9,9 +9,9 @@ from functools import lru_cache
 basedirpath = Path(r"/diskstation")
 videosets = VideosetsII(basedirpath=basedirpath)  # basedirpath)
 classifications = pd.read_csv("classifications.csv")
+
+
 # %%
-
-
 @lru_cache
 def get_tracks(videoset, camera):
     mm = videosets[videoset].get_mediamanager(camera)
@@ -25,6 +25,9 @@ tracks_per_label = defaultdict(list)
 for i, row in classifications.iterrows():
     videoset = row["videoset"]
     camera = row["camera"]
+
     tracks = get_tracks(videoset, camera)
     track = tracks[tracks["track_id"] == row["track_id"]]
     tracks_per_label[row["label"]].append(track)
+
+# %%
